12cc1c8d8cad7c99e109517cb4b462f5c0461121
BEN1803 <benflon004@gmail.com>
2026-07-21 16:42:54 +0300

    initial commit

---
+++ src/main/java/com/dukaniledger/DukaniLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.dukaniledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukaniLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DukaniLedgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dukaniledger/DukaniLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dukaniledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DukaniLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
