de1c75f8167625b1a653bef2e37c9a831f1c2457
BEN1803 <benflon004@gmail.com>
2026-07-27 15:21:42 +0300

    modified backend

--- src/main/java/com/dukaniledger/controller/AuthController.java
+++ src/main/java/com/dukaniledger/controller/AuthController.java
@@ -7,12 +7,10 @@ import com.dukaniledger.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
